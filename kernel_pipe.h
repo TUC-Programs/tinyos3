629622996ded6525305306caaa89086cc295956e
VjonisBoja <149782302+Vboja@users.noreply.github.com>
2024-12-01 14:07:57 +0200

    Update kernel_pipe.h

--- kernel_pipe.h
+++ kernel_pipe.h
@@ -14,7 +14,7 @@ typedef struct pipe_control_block{
     char buffer[PIPE_BUFFER_SIZE];  //bounded cyclic byte buffer
 }PipeCB;
 
-int sys_Pipe(pipe_t* pipe);
+
 int pipe_write(void* pipecb_t, const char *buf, unsigned int n);
 int pipe_read(void* pipecb_t, char *buf, unsigned int n);
 int pipe_writer_close(void* _pipecb);
