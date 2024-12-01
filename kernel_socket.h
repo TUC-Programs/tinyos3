1bf9e564ec33cd46bd322748904bb84f7768264f
VjonisBoja <149782302+Vboja@users.noreply.github.com>
2024-12-01 21:52:33 +0200

    Update kernel_socket.h

--- kernel_socket.h
+++ kernel_socket.h
@@ -6,20 +6,24 @@ typedef struct socket_control_block SCB;
 SCB* PORT_MAP[MAX_PORT]={NULL};
 
 typedef enum{
+
     SOCKET_LISTENER,
     SOCKET_UNBOUND,
     SOCKET_PEER
+
 }Socket_type;
 
 typedef struct listener_socket
 {
     rlnode queue;
     CondVar req_available;
+
 }listener_socket;
 
 typedef struct unbound_socket
 {
     rlnode unbound_socket;
+
 }unbound_socket;
 
 typedef struct peer_socket
@@ -27,8 +31,10 @@ typedef struct peer_socket
     SCB* peer;
     PipeCB* write_pipe;
     PipeCB* read_pipe;
+
 }peer_socket;
 
+/*Struct for socket control block*/
 typedef struct socket_control_block
 {
     uint refcount;
@@ -46,12 +52,15 @@ typedef struct socket_control_block
 
 }SCB;
 
-
+/*Here is the declaration of function that we implement in 
+kernel_socket.c
+*/
 int socket_close(void* socket);
 int socket_read(void* socketcb_t, char *buf, unsigned int n);
 int socket_write(void* socketcb_t, const char *buf, unsigned int n);
 PipeCB* createPipeForAccept(FCB* reader, FCB* writer);
 
+/*Struct for connection request*/
 typedef struct request_connection {
 
   int admitted;
